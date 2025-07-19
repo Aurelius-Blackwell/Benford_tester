1359a242817cb2560404d8e2056c16cc0ff4de17
Robin Blackwell <aureliusblackwell@gmail.com>
2025-07-19 23:26:32 +0100

    Add files via upload

---
+++ massEditor.py
@@ -0,0 +1,15 @@
+import os
+
+source_folder = 'F:/2023_10K_files/'
+destination_folder = 'F:/2023_10K_files_np/'
+
+p = os.listdir(source_folder)
+
+for file in p:
+    filex = open(source_folder+file, 'r', encoding="utf-8")
+    text = filex.read()
+    text = text.replace(',', '') #remove commas
+    text = text.replace('.', '') #remove periods
+    destination = destination_folder+file #no punctuation
+    with open(destination, 'w', encoding="utf-8") as new_file:
+        new_file.write(text)
