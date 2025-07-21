2c1e95f54f72df6af3ca11d7e8aef1d8818bc028
Robin Blackwell <aureliusblackwell@gmail.com>
2025-07-22 00:06:23 +0100

    Update benfordTest.py

--- benfordTest.py
+++ benfordTest.py
@@ -6,18 +6,21 @@ doc = 'benfordTests.xlsx'
 wb = openpyxl.load_workbook(doc)
 sheet = wb['sheetname']
 
+#start at row 4, where the data starts in the Excel template
 row = 4
 for i in range(row, sheet.max_row):
     print(sheet['a'+str(row)].value)
-    #set y variable
+    #set y variable, the numerical frequencies recorded in each Excel row
     y = []
     for cellObj in sheet['c'+str(row):'k'+str(row)]:
         for i in cellObj:
             y.append(i.value)
-    #set x variable
+    #set x variable, Benford's distribution, which will be used to explain the observed numerical frequencies of the data
     x = []
     for i in range(1,10):
         x.append(math.log10((1+i)/i))
+
+    #write stats for each row to their relative column in the Excel template
     slope, intercept, r, p, std_err = stats.linregress(x, y)
     sheet['o'+str(row)].value = float(slope)
     sheet['p'+str(row)].value = float(intercept)
@@ -25,6 +28,9 @@ for i in range(row, sheet.max_row):
     sheet['r'+str(row)].value = float(p)
     sheet['s'+str(row)].value = float(std_err)
     wb.save('benfordTests_copy.xlsx')
+    
+    #move on to next row
     row +=1
 
+#close Excel file to prevent bugs
 wb.close()
